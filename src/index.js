e5e0a2b09b9331e3071fe70d038a2a52d50d6f54
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-30 07:16:54 +0700

    Firebase in React

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import Firebase, {
+  FirebaseContext,
+} from './components/Firebase';
 
-render(<App />, document.getElementById('root'));
+render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root'),
+);
